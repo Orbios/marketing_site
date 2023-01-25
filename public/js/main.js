dc0dc3aa084fa663e34778eb515790047bccdfa7
Andrey Temchenko <temchenko32@gmail.com>
2023-01-25 16:13:23 +0200

    refactor: format with prettier

--- public/js/main.js
+++ public/js/main.js
@@ -1,136 +1,124 @@
-(function() {
+(function () {
+  'use strict';
 
-	"use strict";
+  AOS?.init({
+    ease: 'slide',
+    once: true
+  });
 
-	AOS?.init({
-		ease: 'slide',
-		once: true
-	});
+  var rellax = function () {
+    var rellax = new Rellax('.rellax', {
+      speed: -2,
+      center: false,
+      wrapper: null,
+      round: true,
+      vertical: true,
+      horizontal: false
+    });
+  };
+  rellax();
 
-	var rellax = function(){
-		var rellax = new Rellax('.rellax', {
-	    speed: -2,
-	    center: false,
-	    wrapper: null,
-	    round: true,
-	    vertical: true,
-	    horizontal: false
-	  });
-	}
-	rellax();
+  ////////////////////////////////////
 
-	////////////////////////////////////
+  var slider = function () {
+    var carouselSlider = document.querySelectorAll('.carousel-testimony');
 
-	var slider = function(){
+    if (carouselSlider.length > 0) {
+      var testimonySlider = tns({
+        container: '.carousel-testimony',
+        items: 1,
+        mode: 'carousel',
+        autoplay: true,
+        animateIn: 'tns-fadeIn',
+        animateOut: 'tns-fadeOut',
+        speed: 700,
+        nav: true,
+        gutter: 20,
+        controls: false,
+        autoplayButtonOutput: false,
+        responsive: {
+          0: {
+            items: 1,
+            gutter: 0
+          },
+          600: {
+            items: 2,
+            gutter: 20
+          },
+          1000: {
+            items: 3,
+            gutter: 20
+          }
+        }
+      });
+    }
+  };
+  slider();
 
-		var carouselSlider = document.querySelectorAll('.carousel-testimony');
+  ////////////////////////////////////
 
-		if ( carouselSlider.length > 0 ) {
+  var counter = function () {
+    function countUp(elem) {
+      var current = elem.innerHTML;
 
-			var testimonySlider = tns({
-				container: '.carousel-testimony',
-				items: 1,
-				mode: 'carousel',
-				autoplay: true,
-			  animateIn: 'tns-fadeIn',
-		    animateOut: 'tns-fadeOut',
-				speed: 700,
-				nav: true,
-				gutter: 20,
-				controls: false,
-				autoplayButtonOutput: false,
-				responsive:{
-					0:{
-						items: 1,
-						gutter: 0
-					},
-					600:{
-						items: 2,
-						gutter: 20
-					},
-					1000:{
-						items: 3,
-						gutter: 20
-					}
-				}
-			});
+      var timeIntervalBeforeIncrement = 2000 / elem.getAttribute('data-count');
 
-		}
+      var interval = setInterval(increase, timeIntervalBeforeIncrement);
 
-	}
-	slider();
-	
-	////////////////////////////////////
+      function increase() {
+        elem.innerHTML = current++;
+        if (current > elem.getAttribute('data-count')) {
+          clearInterval(interval);
+        }
+      }
+    }
 
-	var counter = function() {
-		function countUp(elem) {
-			var current = elem.innerHTML;
+    var span = document.querySelectorAll("[id^='count']");
 
+    var i = 0;
+    for (i = 0; i < span.length; i++) {
+      countUp(span[i]);
+    }
+  };
 
-			var timeIntervalBeforeIncrement = 2000/elem.getAttribute("data-count")
+  ////////////////////////////////////
 
+  var elements;
+  var windowHeight;
 
-			var interval = setInterval(increase, timeIntervalBeforeIncrement);
+  function init() {
+    elements = document.querySelectorAll('.ftco-counter-section');
+    windowHeight = window.innerHeight;
+  }
 
-			function increase() {
-				elem.innerHTML = current++;
-				if (current > elem.getAttribute("data-count")) {
-					clearInterval(interval);
-				}
-			}
+  function checkPosition() {
+    var i;
+    for (i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        if (!element.classList.contains('viewed')) {
+          element.classList.add('viewed');
+          counter();
+        } else {
+          if (element.classList.contains('viewed')) {
+          }
+        }
+        // console.log('igo');
+      }
+    }
+  }
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
 
-		}
+  init();
+  checkPosition();
 
-		var span = document.querySelectorAll("[id^='count']");
+  ////////////////////////////////////
 
-		var i = 0;
-		for (i = 0; i < span.length; i++) {
-			countUp(span[i]);
-		}
-	}
-
-	////////////////////////////////////
-
-	var elements;
-	var windowHeight;
-
-	function init() {
-		elements = document.querySelectorAll('.ftco-counter-section');
-		windowHeight = window.innerHeight;
-	}
-
-	function checkPosition() {
-		var i;
-		for (i = 0; i < elements.length; i++) {
-			var element = elements[i];
-			var positionFromTop = elements[i].getBoundingClientRect().top;
-			if (positionFromTop - windowHeight <= 0) {
-				if( !element.classList.contains('viewed') ) {
-					element.classList.add('viewed');
-					counter();	
-				} else {
-					if ( element.classList.contains('viewed') ) {
-
-					}
-				}
-				// console.log('igo');
-
-			}
-		}
-	}
-	window.addEventListener('scroll', checkPosition);
-	window.addEventListener('resize', init);
-
-	init();
-	checkPosition()
-
-	////////////////////////////////////
-
-	const lightbox = GLightbox({
-	  touchNavigation: true,
-	  loop: true,
-	  autoplayVideos: true
-	});
-
-})()
-
+  const lightbox = GLightbox({
+    touchNavigation: true,
+    loop: true,
+    autoplayVideos: true
+  });
+})();
