9b9ae50107aa755a2b2a8cdfb7a7baae712cee1c
Andrey Temchenko <temchenko32@gmail.com>
2023-01-31 11:01:05 +0200

    refactor: add About section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import type {NextPage} from 'next';
 import Layout from 'components/common/Layout';
 import HeroSection from 'components/home/HeroSection';
 import ServicesSection from 'components/home/services/ServicesSection';
+import AboutSection from 'components/home/AboutSection';
 
 const Home: NextPage = () => {
   return (
@@ -11,43 +12,7 @@ const Home: NextPage = () => {
 
       <ServicesSection />
 
-      <section className="ftco-section ftco-about-section ftco-no-pt ftco-no-pb">
-        <div className="container-xl">
-          <div className="row g-xl-5">
-            <div
-              className="col-md-6 d-flex align-items-center"
-              data-aos="fade-up"
-              data-aos-delay="100"
-              data-aos-duration="1000">
-              <img src="images/about.svg" className="img-fluid" alt="" />
-            </div>
-            <div
-              className="col-md-6 py-5 heading-section"
-              data-aos="fade-up"
-              data-aos-delay="200"
-              data-aos-duration="1000">
-              <div className="py-md-5">
-                <span className="subheading">About Us</span>
-                <h2 className="mb-4">Orbios - harmonical grows of your project.</h2>
-                <p>
-                  Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live
-                  the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large
-                  language ocean.
-                </p>
-                <p>
-                  A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a
-                  paradisematic country, in which roasted parts of sentences fly into your mouth.
-                </p>
-                <p>
-                  <a href="#" className="btn btn-primary">
-                    More About us
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <AboutSection />
 
       <section className="ftco-section">
         <div className="container">
