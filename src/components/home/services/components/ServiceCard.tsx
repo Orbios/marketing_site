35310f3134e7ab416f0ac4c67fabb68b6ef90098
Andrey Temchenko <temchenko32@gmail.com>
2023-02-03 14:10:56 +0200

    feat: Items for our services should not be clickable

--- src/components/home/services/components/ServiceCard.tsx
+++ src/components/home/services/components/ServiceCard.tsx
@@ -1,3 +1,5 @@
+import FEATURES from 'constants/features';
+
 interface Props {
   title: string;
   description: string;
@@ -8,21 +10,37 @@ interface Props {
 function ServiceCard({title, description, icon, delay}: Props) {
   const iconToRender = typeof icon === 'string' ? <span className={icon} /> : icon;
 
-  return (
-    <div
-      className="col-md-4 col-lg-4 text-center d-flex align-items-stretch"
-      data-aos="fade-up"
-      data-aos-delay={delay}
-      data-aos-duration="1000">
-      <a href="#" className="services">
+  function renderCardContent() {
+    return (
+      <>
         <div className="icon d-flex align-items-center justify-content-center">{iconToRender}</div>
         <div className="text">
           <h2>{title}</h2>
           <p dangerouslySetInnerHTML={{__html: description}}></p>
         </div>
-      </a>
-    </div>
-  );
+      </>
+    );
+  }
+
+  function render() {
+    return (
+      <div
+        className="col-md-4 col-lg-4 text-center d-flex align-items-stretch"
+        data-aos="fade-up"
+        data-aos-delay={delay}
+        data-aos-duration="1000">
+        {FEATURES.SERVICE_CARD_CLICKABLE ? (
+          <a href="#" className="services">
+            {renderCardContent()}
+          </a>
+        ) : (
+          <div className="services">{renderCardContent()}</div>
+        )}
+      </div>
+    );
+  }
+
+  return render();
 }
 
 export default ServiceCard;
